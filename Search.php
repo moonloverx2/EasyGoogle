d547bc37c22501e7e2f6da2eaeec916750e7544e
moonloverx2 <332322385@qq.com>
2015-12-25 18:44:05 +0800

    新加方法
    新加方法

--- Search.php
+++ Search.php
@@ -1,11 +1,32 @@
 <?php
-$kw = $_POST["kw"];
+//$kw = $_POST["kw"];
 //$url = "https://www.google.com.hk/?gws_rd=cr,ssl#newwindow=1&safe=strict&q=".$kw;
-$url = "https://www.baidu.com/s?wd=%E7%9A%84&rsv_bp=0&ch=&tn=baidu&bar=&rsv_spt=3&ie=utf-8&rsv_enter=1&inputT=970";
+//$url = "https://www.baidu.com/s?wd=%E7%9A%84&rsv_bp=0&ch=&tn=baidu&bar=&rsv_spt=3&ie=utf-8&rsv_enter=1&inputT=970";
 //echo htmlentities(access_url($url));
 //echo curl_file_get_contents($url);
 
-echo  curl("https://www.google.com.hk/?gws_rd=cr,ssl#newwindow=1&safe=strict&q=d", "Mozilla 5.0");
+
+
+
+// 抓取网页URL
+$url = 'http://www.google.com';
+// 初始化，返回一个handler
+$ch = curl_init($url);  
+// 设置选项，有返回值
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// 设置选项，来源页，这意味着可以伪造referer达到某种目的
+curl_setopt($ch, CURLOPT_REFERER, 'http://www.google.cn/');
+// 设置选项，浏览器信息
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; SV1; .NET CLR 1.1.4322)');
+// 执行
+$raw = curl_exec($ch);curl_close($ch);  // 关闭handler
+// 输出结果
+echo $raw;
+
+
+
+
+//echo  curl("https://www.google.com.hk/?gws_rd=cr,ssl#newwindow=1&safe=strict&q=dsfsafsf", "Mozilla 5.0");
   
 
 
@@ -20,7 +41,7 @@ echo  curl("https://www.google.com.hk/?gws_rd=cr,ssl#newwindow=1&safe=strict&q=d
     curl_setopt ($ch, CURLOPT_USERAGENT, $user_agent);
     curl_setopt ($ch, CURLOPT_HEADER, TRUE);
     curl_setopt ($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt ($ch, CURLOPT_REFERER, 'http://www.google.com/');
+   
     curl_setopt ($ch, CURLOPT_COOKIEFILE,"./cookie.txt");
     curl_setopt ($ch, CURLOPT_COOKIEJAR,"./cookie.txt");
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
