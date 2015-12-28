f7ff0e51f5003c10883acc3f491c8b728184fc1f
moonloverx2 <332322385@qq.com>
2015-12-28 17:02:39 +0800

    修改返回结果
    修改返回结果

--- Index.php
+++ Index.php
@@ -1,4 +1,5 @@
-<form action="Search.php" method="get">
+<meta charset="UTF-8">
+<form action="Search.php" method="POST">
 <input type="text" name="kw" id="kw"/>
 <input type="submit" value="提交">
 </form>
--- Search.php
+++ Search.php
@@ -1,69 +1,36 @@
+<meta charset="UTF-8">
 <?php
-//$kw = $_POST["kw"];
-//$url = "https://www.google.com.hk/?gws_rd=cr,ssl#newwindow=1&safe=strict&q=".$kw;
-//$url = "https://www.baidu.com/s?wd=%E7%9A%84&rsv_bp=0&ch=&tn=baidu&bar=&rsv_spt=3&ie=utf-8&rsv_enter=1&inputT=970";
-//echo htmlentities(access_url($url));
-//echo curl_file_get_contents($url);
+$kw = $_POST["kw"];
 
 
-
-
-
-
-
-
-echo  curl("https://www.google.com.hk/?gws_rd=cr,ssl#newwindow=1&safe=strict&q=dsfsafsf", "Mozilla 5.0");
-  
-
-
- function curl($url, $user_agent, $retry=0){
-    if($retry > 5){
-        print "Maximum 5 retries are done, skipping!\n";
-        return "in loop!";
-    }
-
-    $ch = curl_init();
-    curl_setopt ($ch, CURLOPT_URL, $url);
-    curl_setopt ($ch, CURLOPT_USERAGENT, $user_agent);
-    curl_setopt ($ch, CURLOPT_HEADER, TRUE);
-    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, TRUE);
-   
-    curl_setopt ($ch, CURLOPT_COOKIEFILE,"./cookie.txt");
-    curl_setopt ($ch, CURLOPT_COOKIEJAR,"./cookie.txt");
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    $result = curl_exec($ch);
-    curl_close($ch);
-
-    // handling the follow redirect
-    if(preg_match("|Location: (https?://\S+)|", $result, $m)){
-        print "Manually doing follow redirect!\n$m[1]\n";
-        return curl($m[1], $user_agent, $retry + 1);
-    }
-
-    // add another condition here if the location is like Location: /home/products/index.php
-
-    return $result;
+function queryToUrl($query, $start=null, $perPage=100, $country="US") {
+    return "http://www.google.com.hk/search?" . http_build_query(array(
+        // Query
+        "q"     => $query,
+        // Country (geolocation presumably)
+       // "gl"    => $country,
+        // Start offset
+        "start" => $start,
+        // Number of result to a page
+        "num"   => $perPage,
+        "hl"=>"zh-cn"
+    ), true);
 }
 
-function access_url($url) {
-	if ($url=='') return false;
-	$fp = fopen($url, 'r') or exit('Open url faild!');
-	if($fp){
-		while(!feof($fp)) {
-			$file.=fgets($fp)."";
-		}
-		fclose($fp);
-	}
-	return $file;
-}
+// Find first 100 result for "pizza" in Canada
+$ch = curl_init(queryToUrl($kw, 0, 15, "CA"));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_USERAGENT,"Mozilla 5.0");
+curl_setopt($ch, CURLOPT_MAXREDIRS,      4);
+curl_setopt($ch, CURLOPT_TIMEOUT,        5);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
-function curl_file_get_contents($durl){  
-    $ch = curl_init();  
-    curl_setopt($ch, CURLOPT_URL, $durl);  
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true) ; // 获取数据返回    
-    curl_setopt($ch, CURLOPT_BINARYTRANSFER, true) ; // 在启用 CURLOPT_RETURNTRANSFER 时候将获取数据返回    
-    $r = curl_exec($ch);  
-    curl_close($ch);  
-    return $r;  
-}  
+$response = curl_exec($ch);
+
+echo  $response;
+
+
+
 ?>
