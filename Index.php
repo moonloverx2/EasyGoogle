62e69e80ff0ce6c3b6b8c3dac9fbf2f3a453f5a5
moonloverx2 <332322385@qq.com>
2016-01-05 18:01:01 +0800

    添加搜索
    添加搜索

--- Index.php
+++ Index.php
@@ -1,6 +1,9 @@
 <meta charset="UTF-8">
+<script src="jquery-1.9.1.min.js"></script>
 <!--<form action="Search.php" method="POST">-->
 <form action="GetData.php" method="POST">
-<input type="text" name="kw" id="kw"/>
+关键字:<input type="text" name="keyword" id="keyword"/><br/><br/>
+每页数量：<input type="text" name="pagesize" id="pagesize"/><br/><br/>
+页码：<input type="text" name="currentpage" id="currentpage"/>
 <input type="submit" value="提交">
 </form>
