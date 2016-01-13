540306ef3ddf4bdca9e5c3b21d0c38e003defdd3
moonloverx2 <332322385@qq.com>
2016-01-13 15:42:36 +0800

    添加搜索首页面
    添加搜索首页面

--- Search.php
+++ Search.php
@@ -2,14 +2,18 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-    <link href="style.css" rel="stylesheet" type="text/css" />
+    <link href="CSS/style.css" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" type="image/x-icon" href="Images/icon.png" />
     <title>EasyGoogle</title>
 
   </head>
   <body>
+  <div class="g_head"></div>
     <div class="g_searchcontrol">
-    <div><img class="g_logo" src="logo.png"/></div>
-    <div><input type="text" class="g_kwinput" id="keywords" name="keywords"/><input type="submit" class="g_submit" value="Google搜索"/></div>
+    
+    <div><img class="g_logo" src="Images/logo.png"/></div>
+    <div class="g_serchdiv"><input type="text" class="g_kwinput" id="keywords" name="keywords"/><input type="submit" class="g_submit" value="Google搜索"/></div>
     </div>
+    <div class="g_bottom"><a href="#" onclick="javascript:alert('帮助大家提供google搜索。')">关于搜索</a>&nbsp;|&nbsp;意见反馈</a>&nbsp;|&nbsp;E-Mail: MoonLoverX2@163.com</div>
   </body>
 </html>
