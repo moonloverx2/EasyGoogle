9f79ef7b07225a29d172102548d83ec4d560600d
root <root@localhost.localdomain>
2016-01-14 22:19:05 +0800

    添加首页和列表页的搜索功能

--- Index.php
+++ Index.php
@@ -1,18 +1,39 @@
-<meta charset="UTF-8">
-<script src="jquery-1.9.1.min.js"></script>
-<form action="test.php" method="POST">
-<!--<form action="GetData.php" method="POST">-->
-关键字:<input type="text" name="keyword" id="keyword"/><br/><br/>
-每页数量：<input type="text" name="pagesize" id="pagesize"/><br/><br/>
-页码：<input type="text" name="currentpage" id="currentpage"/>
-<input type="submit" value="提交">
-</form>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="keywords" content="Google,EasyGoogle,谷歌地址,能访问google" />
+    <meta name="description" content="国内访问google,帮助大家访问google。http://google.luckyxue.com/。" />
+    <link href="CSS/style.css" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" type="image/x-icon" href="Images/icon.png" />
+    <title>EasyGoogle</title>
+
+  </head>
+  <body>
+  <div class="g_head"></div>
+    <div class="g_searchcontrol">
+    
+    <div><img class="g_logo" src="Images/logo.png"/></div>
+    <form action="Search.php" method="post">
+    <div class="g_serchdiv">
+    <input type="text" class="g_kwinput" id="keyword" name="keyword"/>
+    <input type="submit" class="g_submit" value="Google搜索"/> 
+    </div>
+    </form>
+    </div>
+    <div class="g_bottom">
+        <?php 
+    require_once 'Template/footer.html';
+    ?>
+    </div>
+  </body>
+</html>
 
 <?php 
 
 if(!empty($_COOKIE['host']))
 {
-	echo  $_COOKIE['host'];
+	//echo  $_COOKIE['host'];
 }
 else 
 {
--- Search.php
+++ Search.php
@@ -1,21 +1,63 @@
+    <?php 
+    require 'Helper.php';
+    $keyword = empty($_POST["keyword"])?"EasyGoogle":urlencode($_POST["keyword"]);
+    $pagesize = $_POST["pagesize"]>0?$_POST["pagesize"]:8;
+    $currentpage = $_POST["currentpage"]>0?($_POST["currentpage"]-1)*$pagesize:0;
+	 $de = getArray ($keyword,$currentpage,$pagesize);
+    $count = count($de['responseData']['results']);
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
     <link href="CSS/style.css" rel="stylesheet" type="text/css" />
     <link rel="shortcut icon" type="image/x-icon" href="Images/icon.png" />
-    <title>EasyGoogle</title>
+    <title><?php echo urldecode($keyword);?>_EasyGoogle</title>
 
   </head>
   <body>
-  <div class="g_head"></div>
-    <div class="g_searchcontrol">
+  <div class="l_head">
+  <a href="/"><img class="l_logo" src="Images/logo.png"/></a>
+  <form action="Search.php" method="post">
+  <div class="l_serchdiv">
+  <input type="text" class="l_kwinput" id="keyword" name="keyword" value="<?php echo urldecode($keyword);?>"/>
+  <input type="submit" class="g_submit" value="Google搜索"/>
+  </div>
+  </form>
+  </div>
+    <div class="l_result">
     
-    <div><img class="g_logo" src="Images/logo.png"/></div>
-    <div class="g_serchdiv"><input type="text" class="g_kwinput" id="keywords" name="keywords"/><input type="submit" class="g_submit" value="Google搜索"/></div>
+    <div class="l_left">
+    
+
+    <div class="r_count">
+         找到约 <?php echo $de['responseData']['cursor']['resultCount'];?> 条结果 （用时 <?php echo $de['responseData']['cursor']['searchResultTime'];?> 秒）
     </div>
-    <div class="g_bottom">
-        <?php 
+<?php 
+        for ($i =0;$i<$count;$i++){
+        	echo "<div class=\"r_content\">";
+	        echo "<span class=\"r_title\"><a target=\"blank\" href=\"".urldecode($de['responseData']['results'][$i]['url'])."\" title=\"".$de['responseData']['results'][$i]['titleNoFormatting']."\">".strip_tags($de['responseData']['results'][$i]['title'])."</a></span>";
+	        echo "<div class=\"r_url\">".urldecode($de['responseData']['results'][$i]['url'])."</div>";
+	        echo "<div>".$de['responseData']['results'][$i]['content']."</div>";
+	        
+	        echo "</div>";
+        }
+	
+	?>
+  <div class="f_page">
+  <table class="f_table">
+  <tr>
+  <td class="f_chosed">1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td>
+  </tr>
+  </table>
+  </div> 
+    </div>
+    
+    <div class="l_right"></div> 
+      
+    </div>
+    <div class="l_bottom">
+    <?php 
     require_once 'Template/footer.html';
     ?>
     </div>
